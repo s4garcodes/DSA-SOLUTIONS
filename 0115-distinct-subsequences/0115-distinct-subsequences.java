65559fccb655c2c0fb94b11eaa052d9106c48dd6
sagar <sagarpahwa07@gmail.com>
2026-07-12 18:56:37 +0530

    Time: 18 ms (78.45%), Space: 54.5 MB (41.3%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,36 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
     public int numDistinct(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-        
-        // DP table needs size n x m
-        int[][] dp = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
-        }
-        
-        // BUG FIX: Pass 0-based maximum indices (n-1, m-1)
-        return fun(n - 1, m - 1, s, t, dp);
-    }
-    
-    public int fun(int i, int j, String s, String t, int[][] dp) {
-        // BUG FIX: Check j < 0 first. If the target is fully matched, we found 1 valid subsequence.
-        if (j < 0) return 1;
-        if (i < 0) return 0;
+        int n=s.length();
+        int m=t.length();
+        int[][] dp=new int[n+1][m+1];
+       
+       for(int i=0;i<=n;i++) dp[i][0]=1;
+       for(int j=1;j<=m;j++) dp[0][j]=0;
 
-        // MEMOIZATION: Return already calculated results
-        if (dp[i][j] != -1) return dp[i][j];
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
 
-        if (s.charAt(i) == t.charAt(j)) {
-            // Option 1: Match these characters + Option 2: Skip this character in 's'
-            dp[i][j] = fun(i - 1, j - 1, s, t, dp) + fun(i - 1, j, s, t, dp);
-        } else {
-            // Must skip this character in 's'
-            dp[i][j] = fun(i - 1, j, s, t, dp);
+            if(s.charAt(i-1)==t.charAt(j-1)){
+                dp[i][j]= dp[i-1][j-1] + dp[i-1][j];
+            }
+            else dp[i][j]= dp[i-1][j];
         }
-        
-        return dp[i][j];
+       }
+       return dp[n][m];
     }
 }
