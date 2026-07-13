5103423673512ac6f6366934f6b802acb37f4bf5
sagar <sagarpahwa07@gmail.com>
2026-07-13 09:13:50 +0530

    Time: 6 ms (8.91%), Space: 47 MB (71.6%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -2,26 +2,31 @@ class Solution {
     public int minDistance(String s1, String s2) {
         int n=s1.length();
         int m=s2.length();
-        int[][] dp =new int[n][m];
+        int[][] dp = new int[n+1][m+1];
         for(int i=0;i<n;i++){
             Arrays.fill(dp[i],-1);
         }
-        return fun(n-1,m-1,s1,s2,dp);
-    }
-    private int fun(int i,int j,String s1,String s2,int[][] dp){
-        if(i<0) return 1+j;
-        if(j<0) return 1+i;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        if(s1.charAt(i)==s2.charAt(j)){
-           return dp[i][j]= fun(i-1,j-1,s1,s2,dp); 
+        for(int j=0;j<=m;j++){
+            dp[0][j]=j;
         }
-        else{
-        int insert = 1 + fun(i,j-1,s1,s2,dp);
-        int remove = 1 + fun(i-1,j,s1,s2,dp);
-        int replace = 1 + fun(i-1,j-1,s1,s2,dp);
-        return dp[i][j]= Math.min(insert,Math.min(remove,replace)); 
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
         }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    int insert=1 + dp[i][j-1];
+                    int remove= 1 + dp[i-1][j];
+                    int replace= 1 + dp[i-1][j-1];
+                    dp[i][j]=Math.min(insert,Math.min(remove,replace));
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
